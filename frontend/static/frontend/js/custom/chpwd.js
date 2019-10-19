605c72881130baf7939d0c860716e047de38eb58
Varun Bansal <varun330@gmail.com>
2019-10-19 20:37:08 +0530

    Change Password Frontend fixes

---
+++ frontend/static/frontend/js/custom/chpwd.js
@@ -0,0 +1,22 @@
+function passwordcheck(){
+  var password = document.getElementById("newpassword").value;
+  var repassword = document.getElementById("repassword").value;
+
+  if(repassword != ""){
+    if(password != repassword){
+      document.getElementById("checker").innerHTML = "Passwords don't match";
+      document.getElementById("checker").style.color = "red";
+      return false;
+    }
+    else{
+      document.getElementById("checker").innerHTML = "Passwords match";
+      document.getElementById("checker").style.color = "green";
+      return true;
+    }
+  }
+  else{
+    document.getElementById("checker").innerHTML = "Re-Enter New Password ";
+    document.getElementById("checker").removeAttribute("style")
+    return false
+  }
+}
