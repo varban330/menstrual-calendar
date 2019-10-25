dcbc5aae2899fc30fee6fdc8dbbaca29a24505fb
Varun Bansal <varun330@gmail.com>
2019-10-25 14:31:44 +0530

    Date Checkbox

---
+++ frontend/static/frontend/js/custom/date.js
@@ -0,0 +1,9 @@
+function currently_on(){
+  var x = document.getElementById("pend")
+  if(x.style.display!= "none"){
+    x.style.display= "none"
+  }
+  else{
+    x.style.display = "block"
+  }
+}
