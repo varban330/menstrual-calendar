24be9c19f9e9343baa555c2d4bdfb5eedb56b0d3
Varun Bansal <varun330@gmail.com>
2019-10-25 16:59:25 +0530

    Google Sign in 1

--- frontend/static/frontend/js/custom/login.js
+++ frontend/static/frontend/js/custom/login.js
@@ -8,6 +8,14 @@ document.addEventListener("keyup", function(event) {
   }
 });
 
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 function login_load(){
   var t = window.localStorage.getItem('m-calendar-token');
   if(t){
