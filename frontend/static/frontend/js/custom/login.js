f5ebc34ebad0037cdf88c3bf6f725c99f6c7e27c
Varun Bansal <varun330@gmail.com>
2019-10-26 09:21:09 +0530

    trial google signin

--- frontend/static/frontend/js/custom/login.js
+++ frontend/static/frontend/js/custom/login.js
@@ -10,14 +10,19 @@ document.addEventListener("keyup", function(event) {
 });
 
 function onSignIn(googleUser) {
-  var id_token = googleUser.getAuthResponse().id_token;
-  var profile = googleUser.getBasicProfile();
-  x = id_token
-  console.log('ID: ' + profile.getId());
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
 
 function login_load(){
   var t = window.localStorage.getItem('m-calendar-token');
